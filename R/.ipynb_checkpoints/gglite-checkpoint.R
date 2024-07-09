cc4f3ac0d0f8a13ef8d24d18bbc272bd13fac8cf
miruetoto <miruetoto@gmail.com>
2024-07-09 13:00:01 +0900

    .

--- R/.ipynb_checkpoints/gglite-checkpoint.R
+++ R/.ipynb_checkpoints/gglite-checkpoint.R
@@ -1,45 +1,8 @@
-## sub-functions
 options(jupyter.plot_scale=4)
 options(repr.plot.width=6,repr.plot.height=2.5,repr.plot.res=300)
 figsize <- function(width=6,height=2.5){
     options(repr.plot.width=width,repr.plot.height=height,repr.plot.res=300)
 }
-
-make_df <- function(x, y = NULL) {
-  if (is.null(y)) {
-    y <- x
-    if (!is.vector(y)) {
-      x <- 1:dim(y)[1]
-    } else {
-      x <- 1:length(y)
-    }
-  }
-  if (!is.vector(y)) {
-      dfx <- data.frame(x)
-      dfy <- data.frame(y)
-      df <- cbind(dfx,dfy)
-      df <- tidyr::pivot_longer(df,cols = colnames(dfy), names_to = "label", values_to = "y")
-  } else {
-      df <- data.frame(x=x,y=y)
-  }
-  return(df)
-}
-
-make_args <- function(label, args) {
-  if (!is.null(label) && any(names(args) %in% c("col", "color", "colour", "fill"))) {
-    args[names(args) %in% c("col", "color", "colour", "fill")] <- NULL
-    warning("When the label option is used, the 'color' (or 'colour' or 'col') and 'fill' option will be ignored.")
-  }
-  return(args)
-}
-
-make_geom <- function(df,geom_type,aes,args) {
-  args1 <- list(data = df, mapping = aes)
-  args2 <- args
-  do.call(geom_type, c(args1,args2))    
-}
-
-## gglite
 
 gglite <- function(...){
     ggplot2::ggplot(...)+
@@ -51,18 +14,52 @@ gglite <- function(...){
     ggplot2::theme(plot.margin = ggplot2::unit(c(3,3,0,0), "mm"))
 }
 
+make_df <- function(x, y = NULL) {
+  if (is.null(y)) {
+    y=x
+    if (!is.vector(y)) {
+      x = 1:dim(y)[1]
+    } else {
+      x = 1:length(y)
+    }
+  }
+  if (!is.vector(y)) {
+      dfx = data.frame(x)
+      dfy = data.frame(y)
+      df = cbind(dfx,dfy)
+      df = tidyr::pivot_longer(df,cols = colnames(dfy), names_to = "label", values_to = "y")
+  } else {
+      df = data.frame(x=x,y=y)
+  }
+  return(df)
+}
+
+# check_args <- function(label, args) {
+#   if (!is.null(label) && any(names(args) %in% c("col", "color", "colour","fill"))) {
+#     stop("You cannot use the combination of (label, color) or (label, fill) options because when you use the 'label' option, 'color' or 'fill' is automatically set based on the categories.")
+#   }
+# }
+
+make_geom <- function(df,geom_type,aes,args) {
+  args1 <- list(data = df, mapping = aes)
+  args2 <- args
+  do.call(geom_type, c(args1,args2))    
+}
+
+
 ## main geoms
 
 line <- function(x, y = NULL, label = NULL, ...) {
-  args <- make_args(label,list(...))
+  args <- list(...)
   df <- make_df(x, y)
   geom_type <- ggplot2::geom_line
-  aes <- ggplot2::aes(x = x, y = y, col = label)
+  aes <- ggplot2::aes(x = x, y = y, col = label, group = label)
   make_geom(df,geom_type,aes,args)
 }
 
+
 point <- function(x, y=NULL,label=NULL, ...) {
-  args <- make_args(label,list(...))
+  args <- list(...); check_args(label,args)
   df <- make_df(x, y)
   geom_type <- ggplot2::geom_point
   aes <- ggplot2::aes(x = x, y = y, col = label)
@@ -72,7 +69,7 @@ point <- function(x, y=NULL,label=NULL, ...) {
 ## 2d geoms
 
 smooth <- function(x, y=NULL,label=NULL, ...) {
-  args <- make_args(label,list(...))
+  args <- list(...); check_args(label,args)
   df <- make_df(x, y)
   geom_type <- ggplot2::geom_smooth
   aes <- ggplot2::aes(x = x, y = y, col = label)
@@ -80,7 +77,7 @@ smooth <- function(x, y=NULL,label=NULL, ...) {
 }
 
 step <- function(x, y=NULL,label=NULL, ...) {
-  args <- make_args(label,list(...))
+  args <- list(...); check_args(label,args)
   df <- make_df(x, y)
   geom_type <- ggplot2::geom_step
   aes <- ggplot2::aes(x = x, y = y, col = label)
@@ -88,7 +85,7 @@ step <- function(x, y=NULL,label=NULL, ...) {
 }
 
 jitter <- function(x, y=NULL,label=NULL, ...) {
-  args <- make_args(label,list(...))
+  args <- list(...); check_args(label,args)
   df <- make_df(x, y)
   geom_type <- ggplot2::geom_jitter    
   aes <- ggplot2::aes(x = x, y = y, col = label)
@@ -98,7 +95,7 @@ jitter <- function(x, y=NULL,label=NULL, ...) {
 
 ## 1d geoms
 histogram <- function(y,label=NULL, ...) {
-  args <- make_args(label,list(...))
+  args <- list(...); check_args(label,args)
   df <- make_df(y)
   geom_type <- ggplot2::geom_histogram
   aes <- ggplot2::aes(x = y, y = stat(density), fill = label)
@@ -108,7 +105,7 @@ histogram <- function(y,label=NULL, ...) {
 }
 
 density <- function(y,label=NULL, ...) {
-  args <- make_args(label,list(...))
+  args <- list(...); check_args(label,args)
   df <- make_df(y)
   geom_type <- ggplot2::geom_density
   aes <- ggplot2::aes(x = y, fill = label, col = label)
@@ -117,7 +114,7 @@ density <- function(y,label=NULL, ...) {
 }
 
 qq <- function(y,label=NULL, ...) {
-  args <- make_args(label,list(...))
+  args <- list(...); check_args(label,args)
   df <- data.frame(y)
   geom_type <- ggplot2::geom_qq
   aes <- ggplot2::aes(sample = y, col = label)
@@ -125,7 +122,7 @@ qq <- function(y,label=NULL, ...) {
 }
 
 qq_line <- function(y,label=NULL, ...) {
-  args <- make_args(label,list(...))
+  args <- list(...); check_args(label,args)
   df <- data.frame(y)
   geom_type <- ggplot2::geom_qq_line
   aes <- ggplot2::aes(sample = y, col = label)
@@ -135,7 +132,7 @@ qq_line <- function(y,label=NULL, ...) {
 ## compare geoms
 
 col <- function(x, y=NULL,label=NULL, ...) {
-  args <- make_args(label,list(...))
+  args <- list(...); check_args(label,args)
   df <- make_df(x, y)
   df$x <- as.factor(df$x)
   geom_type <- ggplot2::geom_col  
@@ -145,7 +142,7 @@ col <- function(x, y=NULL,label=NULL, ...) {
 }
 
 boxplot <- function(x, y=NULL,label=NULL, ...) {
-  args <- make_args(label,list(...))
+  args <- list(...); check_args(label,args)
   if (is.null(y)) {
     y=x
     x=0
@@ -157,7 +154,7 @@ boxplot <- function(x, y=NULL,label=NULL, ...) {
 }
 
 violin <- function(x, y=NULL,label=NULL, ...) {
-  args <- make_args(label,list(...))
+  args <- list(...); check_args(label,args)
   if (is.null(y)) {
     y=x
     x=0
--- R/gglite.R
+++ R/gglite.R
@@ -34,11 +34,11 @@ make_df <- function(x, y = NULL) {
   return(df)
 }
 
-check_args <- function(label, args) {
-  if (!is.null(label) && any(names(args) %in% c("col", "color", "colour","fill"))) {
-    stop("You cannot use the combination of (label, color) or (label, fill) options because when you use the 'label' option, 'color' or 'fill' is automatically set based on the categories.")
-  }
-}
+# check_args <- function(label, args) {
+#   if (!is.null(label) && any(names(args) %in% c("col", "color", "colour","fill"))) {
+#     stop("You cannot use the combination of (label, color) or (label, fill) options because when you use the 'label' option, 'color' or 'fill' is automatically set based on the categories.")
+#   }
+# }
 
 make_geom <- function(df,geom_type,aes,args) {
   args1 <- list(data = df, mapping = aes)
@@ -47,11 +47,10 @@ make_geom <- function(df,geom_type,aes,args) {
 }
 
 
-
 ## main geoms
 
 line <- function(x, y = NULL, label = NULL, ...) {
-  args <- list(...); check_args(label,args)
+  args <- list(...)
   df <- make_df(x, y)
   geom_type <- ggplot2::geom_line
   aes <- ggplot2::aes(x = x, y = y, col = label, group = label)
